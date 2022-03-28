5ed736f5b176d16a3b6a48bdd00c82f128c8e456
a17pioja <a17pioja@student.his.se>
2022-03-28 08:26:08 +0200

    Connected PHP to mysql
    Made a successful insert through php

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
     <head>
         <title>Examensarbete</title>
     </head>
-    <body>
-        <?php echo '<h1>Examensarbete a17pioja</h1>'; ?>
+    <body>        
+        <?php
+            $pdo = new PDO('mysql:dbname=a17piojaExamensarbete;host=localhost', 'root', '');
+
+            echo '<h1>Examensarbete a17pioja</h1>'; 
+
+            $querystring='insert into mytable(ID, Name) values (4, "Bill Lucky");';
+            $stmt = $pdo->prepare($querystring);
+            $stmt->execute();
+
+        ?>
     </body>
 </html>
