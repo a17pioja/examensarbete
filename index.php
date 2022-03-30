80b8585499eff4160c440435c12b833a5499938e
a17pioja <a17pioja@student.his.se>
2022-03-30 12:16:12 +0200

    Connected mysql & pgsql to php and made inserts

--- index.php
+++ index.php
@@ -4,14 +4,66 @@
     </head>
     <body>        
         <?php
-            $pdo = new PDO('mysql:dbname=a17piojaExamensarbete;host=localhost', 'root', '');
 
             echo '<h1>Examensarbete a17pioja</h1>'; 
+    
+            if(isset($_POST['enableMysqlBtn'])){
+                EnableMySQL();
+            }
+            
+            if(isset($_POST['enablePgsqlBtn'])){
+                EnablePostgreSQL();
+            }
+            
+            function EnableMySQL() {
+                
+                $dbhost = "localhost";
+                $dbname = "a17piojaExamensarbete";
+                $dbuser = "root";
+                $dbpass = "";
+                
+                try{
+                    $connectMysql = new PDO('mysql:host='.$dbhost.'; dbname='.$dbname, $dbuser, $dbpass);
+                }
+                catch(PDOException $connectionError){
+                    echo "Failed to connect to database: ".$connectionError->getMessage();
+                }
 
-            $querystring='insert into mytable(ID, Name) values (4, "Bill Lucky");';
-            $stmt = $pdo->prepare($querystring);
-            $stmt->execute();
+                $querystring='insert into mytable(Name) values ("Bill Lucky");';
+                $stmt = $connectMysql->prepare($querystring);
+                $stmt->execute();
+                echo "MySQL!";
+            }
 
+            
+            function EnablePostgreSQL() {
+                $dbhost = "localhost";
+                $dbname = "a17piojaExamensarbete";
+                $dbuser = "postgres";
+                $dbpass = "kurwa";
+
+                try{
+                    $connectPgsql = new PDO('pgsql:host='.$dbhost.'; dbname='.$dbname, $dbuser, $dbpass);
+                }
+                catch(PDOException $connectionError){
+                    echo "Failed to connect to database: ".$connectionError->getMessage();
+                }
+
+                $querystring="insert into mytable(Name) values ('Bill Lucky');";
+                $stmt = $connectPgsql->prepare($querystring);
+                $stmt->execute();
+                echo "PostgreSQL!";
+            }
+            
         ?>
+
+            <form method="post">
+            <input type="submit" name="enableMysqlBtn"
+                    class="button" value="mysql" />
+              
+            <input type="submit" name="enablePgsqlBtn"
+                    class="button" value="pqsql" />
+            </form>
+
     </body>
 </html>
