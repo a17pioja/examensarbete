1fbc718d733b9baeb1f43fba4914c0c76825dbee
a17pioja <a17pioja@student.his.se>
2022-03-30 19:44:55 +0200

    Started automating customer creation function

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <body>        
         <?php
 
-            echo '<h1>Examensarbete a17pioja</h1>'; 
-    
             if(isset($_POST['enableMysqlBtn'])){
                 EnableMySQL();
             }
@@ -29,12 +27,9 @@
                     echo "Failed to connect to database: ".$connectionError->getMessage();
                 }
 
-                $querystring='insert into mytable(Name) values ("Bill Lucky");';
-                $stmt = $connectMysql->prepare($querystring);
-                $stmt->execute();
+                CreateCustomersMySQL( $connectMysql);
                 echo "MySQL!";
             }
-
             
             function EnablePostgreSQL() {
                 $dbhost = "localhost";
@@ -54,16 +49,32 @@
                 $stmt->execute();
                 echo "PostgreSQL!";
             }
-            
-        ?>
 
-            <form method="post">
+            function CreateCustomersMySQL($connectMysql){
+                $firstNames = array("Luke", "Danny", "Mike", "Maria", "Julia", "Taylor");
+                $lastNames  = array("Gonzales", "Kowalski", "Smith", "Washington", "Svensson", "Nguyen");
+                $cities  = array("Stochholm", "Warsaw", "Tokyo", "Madrid", "Cario", "Prague");
+                $countries  = array("Sweden", "Poland","Japan", "Spain", "Egypt", "Czechia");
+                $addresses  = array("Lucky st. 13", "Smith st. 68", "Kings st. 13", "Queensway 31", "Sunny st. 26", "Goodneighbour st. 11");
+
+
+                $querystring="insert into Customers(CustomerFirstName, CustomerLastName, CustomerCity, CustomerCountry, 
+                CustomerAddress) values ('" .$firstNames[rand(0,5)]."','".$lastNames[rand(0,5)]."','".$cities[rand(0,5)].
+                "','".$countries[rand(0,5)]."','".$addresses[rand(0,5)]."');";
+
+                $stmt = $connectMysql->prepare($querystring);
+                $stmt->execute();
+            }
+      
+        ?>
+        <h1>Examensarbete a17pioja</h1>
+        <form method="post">
             <input type="submit" name="enableMysqlBtn"
                     class="button" value="mysql" />
               
             <input type="submit" name="enablePgsqlBtn"
                     class="button" value="pqsql" />
-            </form>
+        </form>
 
     </body>
 </html>
